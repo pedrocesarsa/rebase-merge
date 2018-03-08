7c842349ad17311dfe3cd414421b327cd1f3dfb7
Pedro Cesar da Silva Alvares <pedrocesarsa@gmail.com>
2018-03-07 22:07:07 -0300

    Correção para considerar o mês de nascimento.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function age(year, month, day) {
     var d = new Date();
     var age = d.getFullYear() - year;
+    if (month < d.getMonth() + 1) {
+        return age - 1;
+    }
     return age;
 }
